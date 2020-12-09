d42c0c40fcc116dfec0abddd00dd1e1cfb26b598
alexlarin <alex12081973@mail.ru>
2020-12-10 00:52:22 +0300

    lesson6 обработка ошибок 1. оператор retry 2. при неправильно введенном номере 3. при сетевой ошибке, отсутствии сети

--- lesson4/lesson4/ViewModel.swift
+++ lesson4/lesson4/ViewModel.swift
@@ -15,19 +15,28 @@ class ViewModel {
     ) {
         self.apiClient = apiClient
        
-        let networking1 = inputIdentifiersPublisher.map { apiClient.character(id: $0) }.switchToLatest().share()
+        let networking1 = inputIdentifiersPublisher
+            .map { apiClient.character(id: $0) }
+            .switchToLatest()
+            .share()
         self.character = networking1.eraseToAnyPublisher()
         
-        let networking2 = inputIdentifiersPublisher.map { apiClient.location(id: $0) }.switchToLatest().share()
+        let networking2 = inputIdentifiersPublisher
+            .map { apiClient.location(id: $0) }
+            .switchToLatest()
+            .share()
         self.location = networking2.eraseToAnyPublisher()
         
-        let networking3 = inputIdentifiersPublisher.map { apiClient.episode(id: $0) }.switchToLatest().share()
+        let networking3 = inputIdentifiersPublisher
+            .map { apiClient.episode(id: $0) }
+            .switchToLatest()
+            .share()
         self.episode = networking3.eraseToAnyPublisher()
     }
     
     let apiClient: APIClient
+    
     let character: AnyPublisher<Character, NetworkError>
     let location: AnyPublisher<Location, NetworkError>
     let episode: AnyPublisher<Episode, NetworkError>
-    
 }
